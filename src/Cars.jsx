00c81b551d4fd5450afd68e6af5fef069eee7a25
Wellington <junior.wel02@exemplo.br>
2021-08-16 18:36:28 -0300

    exercicio 1

--- src/Cars.jsx
+++ src/Cars.jsx
@@ -1,61 +1,52 @@
-// src/Cars.jsx
-
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
+import Provider from './context/Provider';
 import CarsContext from './context/CarsContext';
 import carBlue from './images/carBlue.jpeg';
 import carRed from './images/carRed.jpeg';
 import carYellow from './images/carYellow.jpeg';
 
-class Cars extends Component {
-  render() {
-    const { redCar, blueCar, yellowCar } = this.context.cars;
-    const { moveCar } = this.context;
-    return (
+function Cars() {
+  const { redCar, blueCar, yellowCar, moveCar } = useContext(CarsContext);
+
+  return (
+    <Provider>
       <div>
         <div>
           <img
             className={redCar ? 'car-right' : 'car-left'}
             src={carRed}
-            alt="red car"
+            alt='red car'
           />
-          <button
-            onClick={() => moveCar('redCar', !redCar)}
-            type="button"
-          >
+          <button onClick={() => moveCar('redCar', !redCar)} type='button'>
             Move
-        </button>
+          </button>
         </div>
         <div>
           <img
             className={blueCar ? 'car-right' : 'car-left'}
             src={carBlue}
-            alt="blue car"
+            alt='blue car'
           />
-          <button
-            onClick={() => moveCar('blueCar', !blueCar)}
-            type="button"
-          >
+          <button onClick={() => moveCar('blueCar', !blueCar)} type='button'>
             Move
-        </button>
+          </button>
         </div>
         <div>
           <img
             className={yellowCar ? 'car-right' : 'car-left'}
             src={carYellow}
-            alt="yellow car"
+            alt='yellow car'
           />
           <button
             onClick={() => moveCar('yellowCar', !yellowCar)}
-            type="button"
+            type='button'
           >
             Move
-        </button>
+          </button>
         </div>
       </div>
-    )
-  }
-};
-
-Cars.contextType = CarsContext;
+    </Provider>
+  );
+}
 
 export default Cars;
